f1c6d2d3380fecb12abc42cf734e7b9f793e734f
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-01-18 16:15:29 +0500

    feat: add nested keyboard button

--- keyboards.py
+++ keyboards.py
@@ -1,30 +1,21 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+# Asosiy menyu tugmasi
+main_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Накладные")]
+    ],
+    resize_keyboard=True
+)
+
+# Накладные tugmasini bosganda ochiladigan keyboard
 delivery_notes = ReplyKeyboardMarkup(
-    keyboard = [
-        [
-            KeyboardButton(text="Январь"),
-            KeyboardButton(text="Февраль"),
-            KeyboardButton(text="Март")
-        ],
-        [
-            KeyboardButton(text="Апрель"),
-            KeyboardButton(text="Май"),
-            KeyboardButton(text="Июнь")
-        ],
-        [
-            KeyboardButton(text="Июль"),
-            KeyboardButton(text="Август"),
-            KeyboardButton(text="Сентябрь")
-        ],
-        [
-            KeyboardButton(text="Октябрь"),
-            KeyboardButton(text="Ноябрь"),
-            KeyboardButton(text="Декабрь")
-        ],
-        [
-            KeyboardButton(text="Главный меню")
-        ]
+    keyboard=[
+        [KeyboardButton(text="Январь"), KeyboardButton(text="Февраль"), KeyboardButton(text="Март")],
+        [KeyboardButton(text="Апрель"), KeyboardButton(text="Май"), KeyboardButton(text="Июнь")],
+        [KeyboardButton(text="Июль"), KeyboardButton(text="Август"), KeyboardButton(text="Сентябрь")],
+        [KeyboardButton(text="Октябрь"), KeyboardButton(text="Ноябрь"), KeyboardButton(text="Декабрь")],
+        [KeyboardButton(text="Главный меню")]  # Orqaga qaytish tugmasi
     ],
     resize_keyboard=True
 )
--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 from aiogram import Bot, Dispatcher, types
 from asyncio import run
-
-import functions
-from functions import *
 from aiogram.types import BotCommand
 from aiogram.filters import Command
 
-dp = Dispatcher()
+import functions  # functions.py faylini import qilish
 
+dp = Dispatcher()
 
 async def startup_answer(bot: Bot):
     await bot.send_message(5323321097, "Bot ishga tushdi! ✅")
 
-
 async def shutdown_answer(bot: Bot):
     await bot.send_message(5323321097, "Bot ishdan toxtadi! ❌")
-
-
-# async def echo(message: types.Message, bot:Bot):
-#     await message.copy_to(chat_id=message.chat.id)
-#     # await message.reply(message.text)
-
 
 async def start():
     dp.startup.register(startup_answer)
 
+    # functions.py dan start_answer va help_answer funksiyalarini chaqiramiz
     dp.message.register(functions.start_answer, Command("start"))
     dp.message.register(functions.help_answer, Command("help"))
-    # dp.message.register(functions.get_user_info)
-    # dp.message.register()
+    dp.message.register(functions.menu_handler, lambda message: message.text in ["Накладные", "Главный меню"])
 
     dp.shutdown.register(shutdown_answer)
 
