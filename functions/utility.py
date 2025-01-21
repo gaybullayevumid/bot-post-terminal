9743cc215d2cb4ea217f09294ee99c9b6fc1663a
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-01-21 12:42:24 +0500

    feat: write data to excel file

--- functions/utility.py
+++ functions/utility.py
@@ -1,6 +1,41 @@
 from aiogram.types import Message
 from keyboards.default.menu_keyboard import *
-from utils.db_api.postgresql import add_user
+import openpyxl
+import psycopg2
+from data.config import DB_CONFIG
+# from utils.db_api.postgresql import add_user
+
+
+def create_excel(month_name):
+    connection = psycopg2.connect(DB_CONFIG)
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+            SELECT * FROM data
+            WHERE month = %s
+        """
+        , (month_name)
+    )
+    records = cursor.fetchall()
+
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = month_name
+
+    headers = ['id', 'name', 'amount', 'date']
+    ws.append(headers)
+
+    for row in records:
+        ws.append(row)
+
+        file_name = f"{month_name}_data.xlsx"
+        wb.save(file_name)
+
+        cursor.close()
+        connection.close()
+
+        return file_name
 
 
 # menu_handler funksiyasi
@@ -9,6 +44,7 @@ async def menu_handler(message: Message):
         await message.reply("Ойларни танланг:", reply_markup=delivery_notes)
     elif message.text == "Главный меню":
         await message.reply("Асосий меню:", reply_markup=main_keyboard)
+    # elif message.text in
 
 # async def registration(message: Message):
 #     if message.text == "Регистрация":
