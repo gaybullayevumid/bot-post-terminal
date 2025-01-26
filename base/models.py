d87fdc2f6a26587d5c67e7338c56f4c2e90259ac
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-01-26 12:33:05 +0500

    feat: add phone number model

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name
